b077397ce2999f0d5da578531724d76b4712634f
MohamedElturky <153658210+MohamedElturky@users.noreply.github.com>
2024-11-26 16:21:32 +0200

    building the front end and the back end

---
+++ back-end/src/main/java/com/Paint/Paint/project/PaintProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Paint.Paint.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintProjectApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/Paint/Paint/project/PaintProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Paint.Paint.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
